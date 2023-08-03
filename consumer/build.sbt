861389b2f87205545efcee7fa6e84c987b9e82ea
Guilherme Samuel <guilherme.siqueira@caf.io>
2023-08-02 23:24:34 -0300

    feat: add functions to serialize and desserialize json files

--- consumer/build.sbt
+++ consumer/build.sbt
@@ -13,4 +13,5 @@ libraryDependencies ++= Seq(
   "org.apache.flink" % "flink-clients" % "1.16.1",
   "org.apache.flink" %% "flink-streaming-scala" % "1.16.1" % "provided",
   "org.slf4j" % "slf4j-simple" % "2.0.5",
+  "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.3"
 )
---
+++ consumer/src/main/scala/utils/Json.scala
@@ -0,0 +1,30 @@
+package org.xqdl
+package utils
+
+import protocols.RawSale
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+
+object Json {
+  def deserialize(jsonString: String): RawSale = {
+    // ObjectMapper for JSON parsing
+    val objectMapper = new ObjectMapper()
+    objectMapper.registerModule(DefaultScalaModule)
+
+    // Deserialize the JSON string into a case class instance
+    objectMapper.readValue(jsonString, classOf[RawSale])
+  }
+
+  def serialize(sale: RawSale): String = {
+    val objectMapper = new ObjectMapper()
+    objectMapper.registerModule(DefaultScalaModule)
+
+    // Serialize the case class instance to a JSON string
+    val jsonString: String = objectMapper.writeValueAsString(sale)
+
+    // Now you have the JSON string
+    jsonString
+    // Output: {"name":"John Doe","age":30,"email":"johndoe@example.com"}
+  }
+}
